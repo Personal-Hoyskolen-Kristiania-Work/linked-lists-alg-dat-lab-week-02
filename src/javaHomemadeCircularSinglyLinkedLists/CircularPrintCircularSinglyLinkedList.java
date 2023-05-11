ca34c0706c08008d6fc8d7c480de4d5c6b142019
Pål-Øivind Mentveit <pame004@student.kristiania.no>
2023-05-11 15:31:07 +0200

    fixed traversing for printing array, now next flaw seem to be involved with deletion

--- src/javaHomemadeCircularSinglyLinkedLists/CircularPrintCircularSinglyLinkedList.java
+++ src/javaHomemadeCircularSinglyLinkedLists/CircularPrintCircularSinglyLinkedList.java
@@ -3,10 +3,14 @@ package src.javaHomemadeCircularSinglyLinkedLists;
 public class CircularPrintCircularSinglyLinkedList {
     public void traversePrintingLL(CircularSinglyLinkedList LL) {
         CircularSinglyNode temp = LL.head;
-        while (temp.getNext() != LL.head) {
+        boolean stopCondition = false;
+        while (stopCondition == false) {
             // non ln print, to print out the data in linked lists
             System.out.print(temp.getData() + " -> "); //print out node data, and an arrow for next line
             temp = temp.getNext(); //set current node to next node
+            if(temp.getNext() == LL.head.getNext()){
+                stopCondition = true;
+            }
         }
         System.out.println("NULL");
     }
